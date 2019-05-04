3b5e76b5c273338e09f8d4e9e1be53916b1d60fc
Kateryna <silchencko@gmail.com>
2019-05-04 15:29:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const container = document.querySelector(".PetShop");
 function getRequest() {
     //debugger;
     return fetch('data.json')
-   // return fetch('model/data.json')
+   // return fetch('https://silchencko.github.io/data.json')
     .then(response => response.json())
     .then(data => {
         console.log(data)
